1ffce2fa7c2fa446ab956655c312f7d40b84bce2
Nathan Steed <steedn@outlook.com>
2020-08-16 04:22:02 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -13,6 +13,6 @@ class BankAccount
     "Your balance is $#{@balance}."
   end
   def valid?
-    self.status == "open" || self.balance > 0
+    self.status == "open" && self.balance > 0
   end
 end
