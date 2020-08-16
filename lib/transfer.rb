1000d437381b6ae5d8f5a40a27e662d053332de6
Nathan Steed <steedn@outlook.com>
2020-08-16 04:47:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,11 +16,12 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    else 
+    else
       reject
     end
   end
-  def reject 
+  def reject
     self.status = "rejected"
     "Transaction rejected. Please check your account balance."
+  end
 end
