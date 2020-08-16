06d867716ccfbc9566ab48f15e629553871c2961
Nathan Steed <steedn@outlook.com>
2020-08-16 04:24:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,5 +2,7 @@ class Transfer
   attr_accessor :sender, :receiver, :amount
   def initialize(payee, recipient, amount)
     @sender = payee
+    @receiver = recipient
+    @amount = amount
   end
 end
