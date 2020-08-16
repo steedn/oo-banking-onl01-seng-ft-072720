cf555b215dadddbb1a72b55708996c119b5bbded
Nathan Steed <steedn@outlook.com>
2020-08-16 04:36:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,11 +14,11 @@ class Transfer
   def execute_transaction
     counter = 0
     while counter < 1
-    @sender.balance -= amount
-    @receiver.balance += amount
-    @status = "complete"
-    # binding.pry
-    counter +=1
-  end
+      @sender.balance -= amount
+      @receiver.balance += amount
+      @status = "complete"
+      # binding.pry
+      counter +=1
+    fend
   end
 end
